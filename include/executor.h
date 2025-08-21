0415bdf7d8f89cec69570cdbb8dd4eaafe2fad56
TarekRemo <tarekrummo64@gmail.com>
2025-08-21 21:13:47 +0200

    fixing canonical mode bug + adding code comments

--- include/executor.h
+++ include/executor.h
@@ -3,6 +3,17 @@
 
 #include "../include/parser.h"
 
+/**
+ * @brief Executes a command using the execvp function.
+ * This function forks a new process and executes the command in the child process.
+ * It handles command options and arguments, and returns the result of the execution.
+ * If the command execution fails, it prints an error message to stderr.
+ * @param command The command structure containing the command name, options, and arguments.
+ * @return Returns the exit status of the command if it was executed successfully,
+ *  '-1' if the execvp fails with an error, '-2' if the fork fails, 
+ * '-3' if the child process is terminated by a signal,
+ *  or '-4' if the child process was stopped.
+ */
 int execute(command command); 
 
 #endif
--- src/executor.c
+++ src/executor.c
@@ -1,5 +1,6 @@
 #include "../include/executor.h"
 #include "../include/parser.h"
+#include "../include/utils.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -11,42 +12,47 @@
 int execute(command command){
 
     char* executableName;
-    char* slash;  
+    char* slash; 
     int result = 0;
 
-    slash = strrchr(command.name, '/'); 
+    //getting the pointer of the last slash in the command's name
+    slash = strrchr(command.name, '/');
 
-    if(slash == NULL){
+    if(slash == NULL){ //if there is no slash in the name
         executableName = command.name; 
     }
     else{
-        executableName = slash+1;
+        executableName = slash+1; //the last file of the path is the executable
     }
 
+    //array containing every arg and opt necessary for the 'execvp' function
     char* argsAndOpts[command.argsNum+command.optionsNum+2]; 
     argsAndOpts[0] = strdup(executableName);
 
+    //filling the array with the arguments
     for(int i = 0 ; i < command.argsNum ; i++){
         argsAndOpts[i+1] = strdup(command.args[i]); 
     }
 
+    //filling the array with the options
     for(int i = 0 ; i < command.optionsNum ; i++){
         argsAndOpts[command.argsNum+1+i] = strdup(command.options[i]); 
     }
 
-    
+    //necessary for the 'execvp' function
     argsAndOpts[command.argsNum+command.optionsNum+1] = NULL;  
 
     pid_t pid = fork(); 
 
     switch(pid){
-        case -1 : 
+        case -1 : //if the creation of the child process faild 
             perror("impossible de créer un processus fils pour exécuter la commande"); 
             result = -2; 
             break;
 
-        case 0 :
-            result = execvp(command.name, argsAndOpts); 
+        case 0 : //child process
+            set_canonical_mode(STDIN_FILENO, 1); //enabling canonical mode
+            result = execvp(command.name, argsAndOpts); //executing the command
             if(result == -1){
                 switch(errno){
                     case EACCES : 
@@ -77,12 +83,16 @@ int execute(command command){
             }
             break;
 
-        default :
+        default : //parent process
             int status; 
-            waitpid(pid, &status, 0); 
+            waitpid(pid, &status, 0); //waiting for the child process
+            set_canonical_mode(STDIN_FILENO, 0); //disabling canonical mode
             if(WIFEXITED(status) != 0){
-                if(WEXITSTATUS(status) == EXIT_FAILURE)
+                int returnedValue = WEXITSTATUS(status);
+                if( returnedValue == EXIT_FAILURE)
                     result = -1; 
+                else 
+                    result = returnedValue;
             }
 
             else if(WIFSIGNALED(status) != 0){
